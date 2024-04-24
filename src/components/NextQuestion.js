de1db12814df1fd53b576b5354cb964a893ae142
Fırat Can Taş <firatcantass@gmail.com>
2024-04-24 09:44:58 +0300

    🌱 update NextQuestion config

--- src/components/NextQuestion.js
+++ src/components/NextQuestion.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const NextQuestion = ({dispatch, answer}) => {
+const NextQuestion = ({dispatch, answer, index, numberOfQuestions}) => {
     if (answer === null) {
         return null
+    }
+    if (index === numberOfQuestions - 1) {
+      return (
+        <button className='btn btn-ui' onClick={() => dispatch({type: "finish"})}>Finish</button>
+      )
     }
   return (
     <button className='btn btn-ui' onClick={() => dispatch({type: "nextQuestion"})}>Next</button>
